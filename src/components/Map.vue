28e2e42f06e818b8a57da1d008d59f28003397a3
Daniel Darabos <darabos.daniel@gmail.com>
2025-06-11 16:11:58 +0200

    More mapping, boss icons.

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -34,7 +34,7 @@ const allPoints: Point[] = [
   { x: 409, y: 484, type: "combat", name: "Wild Slime" },
   {
     x: 409, y: 419, type: "combat", name: "Animated Skeleton",
-    next: { 'Turn right': 'right1', 'Turn left': 'left1', 'Go straight': 'straight1' }
+    next: { 'Turn right': 'right1', 'Turn left': 'left1', 'Go straight': 'straight1' },
   },
   { x: 455, y: 419, type: "pickup", name: "fruit", label: 'right1' },
   { x: 501, y: 419, type: "pickup", name: "gem" },
@@ -48,17 +48,64 @@ const allPoints: Point[] = [
   { x: 350, y: 461, type: "combat", end: true },
 
   { x: 409, y: 359, type: "pickup", name: "rescue", label: 'straight1' },
-  { x: 347, y: 359, type: "pickup", name: "campfire" },
+  { x: 342, y: 359, type: "pickup", name: "campfire" },
   { x: 288, y: 359, type: "none" },
   { x: 288, y: 343, type: "combat" },
   { x: 288, y: 293, type: "combat" },
-  { x: 288, y: 236, type: "combat" },
-  { x: 288, y: 219, type: "none" },
-  { x: 351, y: 219, type: "combat", end: true },
+  {
+    x: 288, y: 236, type: "combat",
+    next: { 'Turn left': 'left2', 'Go straight': 'straight2' },
+  },
+  { x: 288, y: 219, type: "none", label: 'straight2' },
+  { x: 351, y: 219, type: "combat" },
+  { x: 351, y: 244, type: "none" },
+  { x: 332, y: 244, type: "none" },
+  { x: 332, y: 279, type: "combat" },
+  { x: 332, y: 318, type: "combat" },
+  { x: 358, y: 318, type: "none" },
+  { x: 358, y: 289, type: "none" },
+  { x: 405, y: 289, type: "combat", end: true },
+
+  { x: 288, y: 243, type: "none", label: 'left2' },
+  { x: 248, y: 243, type: "combat" },
+  { x: 243, y: 243, type: "none" },
+  { x: 243, y: 341, type: "combat" },
+  { x: 243, y: 379, type: "none" },
+  { x: 277, y: 379, type: "none" },
+  {
+    x: 277, y: 413, type: "none",
+    next: { 'Turn left': 'left3', 'Go straight': 'straight3' },
+  },
+  { x: 243, y: 413, type: "combat", label: 'left3', end: true },
+  {
+    x: 277, y: 459, type: "none", label: 'straight3',
+    next: { 'Turn left': 'left4', 'Go straight': 'straight4' },
+  },
+  { x: 243, y: 459, type: "combat", label: 'left4', end: true },
+  { x: 277, y: 498, type: "none", label: 'straight4' },
+  {
+    x: 196, y: 498, type: "none",
+    next: { 'Go straight': 'straight5', 'Turn right': 'right5' },
+  },
+  { x: 196, y: 460, type: "combat", label: 'right5' },
+  { x: 196, y: 410, type: "combat" },
+  { x: 196, y: 350, type: "combat" },
+  { x: 196, y: 300, type: "combat", end: true },
+  {
+    x: 126, y: 498, type: "none", label: 'straight5',
+    next: { 'Go straight': 'straight6', 'Turn right': 'right6' },
+  },
+  { x: 126, y: 466, type: "combat", label: 'right6' },
+  { x: 126, y: 390, type: "pickup", name: 'fruit', end: true },
+  { x: 68, y: 498, type: "none", label: 'straight6' },
+  { x: 68, y: 323, type: "none" },
+  { x: 124, y: 323, type: "pickup", name: 'boss' },
+
+  { x: 407, y: 127, type: "pickup", name: 'finalboss' },
 ];
 const points = computed(() => {
   const points = [] as Point[];
-  const steps = ['Go straight'];
+  const steps = ['Go straight', 'Turn left', 'Go straight', 'Go straight', 'Go straight', 'Go straight'];
   let target = undefined as string | undefined;
   for (const point of allPoints) {
     if (target && point.label === target) {
@@ -90,13 +137,18 @@ function style(point: Point) {
   if (!mapElement.value) return { display: 'none' };
   const x = point.x * scale.value;
   const y = point.y * scale.value;
-  const w = 30 * scale.value;
-  const h = 30 * scale.value;
+  let s = 30 * scale.value;
+  if (point.name === 'boss') {
+    s *= 1.2;
+  }
+  if (point.name === 'finalboss') {
+    s *= 2;
+  }
   return {
-    left: `${x - w / 2}px`,
-    top: `${y - h / 2}px`,
-    width: `${w}px`,
-    height: `${h}px`,
+    left: `${x - s / 2}px`,
+    top: `${y - s / 2}px`,
+    width: `${s}px`,
+    height: `${s}px`,
   };
 }
 
