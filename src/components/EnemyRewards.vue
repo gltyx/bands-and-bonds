f7566e3ec2ecece602f2fee3895a79b0585bb56c
Daniel Darabos <darabos.daniel@gmail.com>
2025-06-24 22:01:30 +0200

    Add files I forgot.

---
+++ src/components/EnemyRewards.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+import type { Enemy } from '../store.ts';
+import Gold from './Gold.vue';
+import Fruit from './Fruit.vue';
+const props = defineProps<{
+  enemy: Enemy;
+}>();
+</script>
+
+<template>
+  <Gold v-if="props.enemy.rewards?.gold" :amount="props.enemy.rewards?.gold" />
+  <Fruit v-if="props.enemy.rewards?.fruit" :amount="props.enemy.rewards?.fruit" />
+</template>
---
+++ src/components/Gold.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+const props = defineProps<{
+  amount: number;
+}>();
+</script>
+
+<template>
+  <span class="numbers">{{ props.amount }}
+    <img src="/images/generated/gold.webp" class="resource-icon" /></span>
+</template>
