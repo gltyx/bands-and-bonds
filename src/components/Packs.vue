ee17611d6432425aeb2e7d3d515cd7dbde5a919a
Daniel Darabos <darabos.daniel@gmail.com>
2025-06-27 20:45:38 +0200

    Add forgotten Packs.vue.

---
+++ src/components/Packs.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+import { numberFormat } from '../store.ts';
+const props = defineProps<{
+  amount: number;
+}>();
+</script>
+
+<template>
+  <span class="numbers">{{ numberFormat.format(props.amount) }}
+    <img src="/images/generated/pack.webp" class="resource-icon" /></span>
+</template>
